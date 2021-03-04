aa4433deacc551ce06502a1f70b84e7b995978d4
Kotaina <kotaina217+git@gmail.com>
2021-03-04 13:33:58 +0300

    features

--- src/blocks/chat-item.js
+++ src/blocks/chat-item.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import "../css/Chat-item.scss"
+import "../css/Chat-item.scss";
+import workImage from "../img/Shape.png";
+import fitnessImage from "../img/Shape2.png";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
+
+    const images = [workImage, fitnessImage]
+
     return (
         <section className="Chat-item"
             id={props.index}
-            onClick={props.onItemClick}>
-            Chat
-        </section>
+            onClick={props.onItemClick}
+            style={{
+                backgroundImage: `url(${images[props.index]})`,
+                backgroundSize: "100 %",
+                // eslint-disable-next-line no-dupe-keys
+                backgroundSize: "cover"
+            }}
+        />
     )
 }
